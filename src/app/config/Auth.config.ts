3e1616d2b87f8f88fdb7e304c627f928379bd7ea
Remy <remy.tupenot@gmail.com>
2021-08-02 02:10:25 +0200

    Readme

--- src/app/config/Auth.config.ts
+++ src/app/config/Auth.config.ts
@@ -5,8 +5,14 @@ export default class AuthConfig {
     
     constructor(){
 
-        this.privateKey = JSON.parse(process.env.PRIVATE_KEY!);
-        this.publicKey = JSON.parse(process.env.PUBLIC_KEY!);
+        /* HEROKU PROD */
+        /*this.privateKey = JSON.parse(process.env.PRIVATE_KEY!);
+        this.publicKey = JSON.parse(process.env.PUBLIC_KEY!);*/
+
+        /* LOCAL DEV*/
+        this.privateKey = process.env.PRIVATE_KEY;
+        this.publicKey = process.env.PUBLIC_KEY;
+
     }
 
     getPrivateKey() : string {
